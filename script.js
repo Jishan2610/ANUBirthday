e5d70a3f20440e92204ff1d0b3bfdffe12366e08
Jishan2610 <jishan4all@gmail.com>
2021-05-28 18:35:40 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "ANU" && b=="05.06.2001")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
